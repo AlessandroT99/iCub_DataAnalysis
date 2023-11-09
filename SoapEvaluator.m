02f1cad60b9a2f133064f10c7cee1e59dd65244a
AlessandroT99 <62152778+AlessandroT99@users.noreply.github.com>
2023-11-09 17:13:04 +0100

    Update SoapEvaluator.m

--- SoapEvaluator.m
+++ SoapEvaluator.m
@@ -28,7 +28,7 @@ numPeople = 32;
 people = readtable("..\InputData\Dati Personali EXP2.xlsx");
 people = people(1:numPeople,:);
 
-soapWidth = people.WidthSoap(1);            % mm
+soapWidth = people.WidthSoap;               % mm
 soapHeight = people.HeightSoap(1);          % mm
 lateralHeight = people.LateralHeight(1);    % mm, it represents the height from the bottom to the line at the half of the soap
 flatWidth = people.FlatWidth(1);            % mm, it represents the flat part on the top of the soap
@@ -45,66 +45,73 @@ flatWidth = people.FlatWidth(1);            % mm, it represents the flat part on
 
 %% Indetation angle evaluation
 a = people.DeltaIndent;
-c = people.Width;
+c = people.CutWidth;
 b = sqrt(a.^2 + c.^2);
 % c = b*cos(theta)
 angle = acos(c./b).*180./pi; % DEGREES
 
 %% Evaluation of the distance from the cut to the top of the soap
-topPointsX = [0,(soapWidth-flatWidth)/2,soapWidth/2,soapWidth-(soapWidth-flatWidth)/2,soapWidth];
-topPointsY = [lateralHeight,soapHeight,soapHeight,soapHeight,lateralHeight];
-p = polyfit(topPointsX, topPointsY, 2);
-xSoapWidth = linspace(0,soapWidth,1000);
-ySoapWidth = polyval(p,xSoapWidth);
-
+RigthDistanceFromSoapTop = zeros(1,numPeople);
+LeftDistanceFromSoapTop = zeros(1,numPeople);
 for i = 1:numPeople
+    x1 = [0,(soapWidth(i)/2-flatWidth)/2];
+    x2 = [soapWidth(i)-flatWidth/2,soapWidth(i)];
+    y1 = [lateralHeight,soapHeight];
+    y2 = [soapHeight,lateralHeight];
+    topPointsX = [x1,soapWidth(i)/2,x2];
+    topPointsY = [y1,soapHeight,y2];
+    xSoapWidth = linspace(0,soapWidth(i),1000);
+    ySoapWidth = polyval(p,xSoapWidth);
+    figure, plot(xSoapWidth,ySoapWidth)
     if strcmp(people.Mano(i),"DX") == 1
-        RigthDistanceFromSoapTop = ySoapWidth(round((soapWidth/2-people.Width(i)/2)*10))-people.Human(i);
-        LeftDistanceFromSoapTop = ySoapWidth(round((soapWidth/2+people.Width(i)/2)*10))-people.Robot(i);
+        RigthDistanceFromSoapTop(i) = ySoapWidth(round((soapWidth(i)/2-people.CutWidth(i)/2)*10));%-people.HumanSide(i);
+        LeftDistanceFromSoapTop(i) = ySoapWidth(round((soapWidth(i)/2+people.CutWidth(i)/2)*10));%-people.RobotSide(i);
     else
-        RigthDistanceFromSoapTop = ySoapWidth(round((soapWidth/2-people.Width(i)/2)*10))-people.Robot(i);
-        LeftDistanceFromSoapTop = ySoapWidth(round((soapWidth/2+people.Width(i)/2)*10))-people.Human(i);
+        RigthDistanceFromSoapTop(i) = ySoapWidth(round((soapWidth(i)/2-people.CutWidth(i)/2)*10));%-people.RobotSide(i);
+        LeftDistanceFromSoapTop(i) = ySoapWidth(round((soapWidth(i)/2+people.CutWidth(i)/2)*10));%-people.HumanSide(i);
     end
 end
 
 %% Evaluation of the removed area
 removedArea = zeros(1,numPeople);
 for i = 1:numPeople
-    cutWidth = [soapWidth/2+people.Width(i)/2, soapWidth/2+people.Width(i)/2];
-    if strcmp(people.Name(i),"DX") == 1
-        removedArea(i) = trapz(xSoapWidth,ySoapWidth-interp1(cutWidth,[people.Robot(i), people.Human(i)]),linspace(cutWidth(1),cutWidth(2),1000));
+    cutWidth = [soapWidth(i)/2-people.CutWidth(i)/2, soapWidth(i)/2+people.CutWidth(i)/2];
+    if strcmp(people.Mano(i),"DX") == 1
+        removedArea(i) = trapz(xSoapWidth,ySoapWidth-interp1(cutWidth,[people.RobotSide(i), people.HumanSide(i)],linspace(cutWidth(1),cutWidth(2),1000)));
     else
-        removedArea(i) = trapz(xSoapWidth,ySoapWidth-interp1(cutWidth,[people.Human(i), people.Robot(i)]),linspace(cutWidth(1),cutWidth(2),1000));
+        removedArea(i) = trapz(xSoapWidth,ySoapWidth-interp1(cutWidth,[people.HumanSide(i), people.RobotSide(i)],linspace(cutWidth(1),cutWidth(2),1000)));
     end
 end
 
 %% Plot results
 fig1 = figure('Name','Right hand soap indentation');
+fig1.WindowState = 'maximized';
 sgtitle('Soap Indentation - Right Hand tests'), hold on
 fig2 = figure('Name','Left hand soap indentation');
+fig2.WindowState = 'maximized';
 sgtitle('Soap Indentation - Left Hand tests'), hold on
 rPeople = 0;
 lPeople = 0;
 for i = 1:numPeople
-    if strcmp(people.Name(i),"DX") == 1
+    if strcmp(people.Mano(i),"DX") == 1
         rPeople = rPeople + 1;
         figure(fig1)
-        subplot(numPeople/4,numPeople/4,rPeople), hold on
-        plot([soapWidth/2-people.Width(i)/2, soapWidth/2+people.Width(i)/2], [people.Robot(i), people.Human(i)],'r-')
-        plot([soapWidth/2+people.Width(i)/2, soapWidth/2+people.Width(i)/2], [people.Human(i), RigthDistanceFromSoapTop(i)],'k--')
-        plot([soapWidth/2-people.Width(i)/2, soapWidth/2-people.Width(i)/2], [people.Robot(i), LeftDistanceFromSoapTop(i)],'k--')
+        subplot(numPeople/8,numPeople/8,rPeople), hold on
+        plot([soapWidth(i)/2-people.CutWidth(i)/2, soapWidth(i)/2+people.CutWidth(i)/2], [people.RobotSide(i), people.HumanSide(i)],'r-')
+        plot([soapWidth(i)/2+people.CutWidth(i)/2, soapWidth(i)/2+people.CutWidth(i)/2], [people.HumanSide(i), RigthDistanceFromSoapTop(i)],'k--')
+        plot([soapWidth(i)/2-people.CutWidth(i)/2, soapWidth(i)/2-people.CutWidth(i)/2], [people.RobotSide(i), LeftDistanceFromSoapTop(i)],'k--')
     else
         lPeople = lPeople + 1;
         figure(fig2)
-        subplot(numPeople/4,numPeople/4,lPeople), hold on
-        plot([soapWidth-people.Width(i)/2, soapWidth+people.Width(i)/2], [people.Human(i), people.Robot(i)],'b-')
-        plot([soapWidth/2+people.Width(i)/2, soapWidth/2+people.Width(i)/2], [people.Robot(i), RigthDistanceFromSoapTop(i)],'k--')
-        plot([soapWidth/2-people.Width(i)/2, soapWidth/2-people.Width(i)/2], [people.Human(i), LeftDistanceFromSoapTop(i)],'k--')
+        subplot(numPeople/8,numPeople/8,lPeople), hold on
+        plot([soapWidth(i)/2-people.CutWidth(i)/2, soapWidth(i)/2+people.CutWidth(i)/2], [people.HumanSide(i), people.RobotSide(i)],'b-')
+        plot([soapWidth(i)/2+people.CutWidth(i)/2, soapWidth(i)/2+people.CutWidth(i)/2], [people.RobotSide(i), RigthDistanceFromSoapTop(i)],'k--')
+        plot([soapWidth(i)/2-people.CutWidth(i)/2, soapWidth(i)/2-people.CutWidth(i)/2], [people.HumanSide(i), LeftDistanceFromSoapTop(i)],'k--')
     end
     plot(xSoapWidth,ySoapWidth,'k')
-    xlim([0, soapWidth])
-    ylim([0, soapHeight])
-    titleName = strjoin(["Test N. ", num2str(i), "  -  Indentation angle: ", num2str(angle(i))],"");
+    xlim([0, soapWidth(i)])
+    ylim([0, soapHeight+1])
+    titleName = strjoin(["Test N. ", num2str(i), " - Indentation angle: ", num2str(angle(i)), " [deg]"],"");
     title(titleName)
     hold off
 end
